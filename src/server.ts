e0c6b41eb7452e2fd80f7c92ccf1ef216ce7ed2d
Md. Shajid Rahman <shajiedrahman@gmail.com>
2026-05-18 17:43:55 +0600

    manupulate the express request and response

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
-import express from "express"
-const app = express()
+import express, { type Application, type Request, type Response } from "express"
+const app : Application = express()
 const port = 5000
-
-app.get('/user', (req, res) => {
-  res.send('Nodejs is running')
+app.use(express.json());
+app.get('/', (req :Request, res:Response) => {
+//   res.send('Nodejs is running')
+res.status(200).json({  name: "John Doe",
+  email: "john.doe@example.com" 
 })
+});
+app.post("/",async(req:Request,res:Response)=>{
+     console.log(req.body);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
