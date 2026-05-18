d0496e658b53271f5beaff0db147672399871817
Md. Shajid Rahman <shajiedrahman@gmail.com>
2026-05-18 18:40:21 +0600

    executed pool and created a table by sql

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,29 @@ app.use(express.text());
 app.use(express.urlencoded({extended:true}));
 
 const pool = new Pool({
-    connectionString:"postgresql://neondb_owner:npg_N4PMileXa1Bo@ep-old-art-apyn5y97-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+    connectionString:
+    "postgresql://neondb_owner:npg_N4PMileXa1Bo@ep-old-art-apyn5y97-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 })
+const initDB=async()=>{
+    try{
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        birth_year INT,
+        country VARCHAR(100),
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
 
+);
+            `)
+            console.log("Database initialized successfully");
+    }catch(error){
+        console.error("Error initializing database:",error);
+    }
+}
 
-
+initDB();
 app.get('/', (req :Request, res:Response) => {
 //   res.send('Nodejs is running')
 res.status(200).json({  name: "John x Doe",
