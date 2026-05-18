a3f24f70689c04f8f5e6e848b26d91331c40a2af
Md. Shajid Rahman <shajiedrahman@gmail.com>
2026-05-19 00:51:36 +0600

    add single users to find by id by get method

--- src/server.ts
+++ src/server.ts
@@ -66,7 +66,32 @@ app.get("/api/users",async(req:Request,res:Response)=>{
             error:error,
         })
     }
-})
+});
+app.get("/api/users/:id",async(req:Request,res:Response)=>{
+    const {id}=req.params;
+    try{
+        const result=await pool.query(`
+        SELECT * FROM users WHERE id=$1
+        `,[id]);
+        if(result.rows.length===0){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            });
+        }
+        res.status(200).json({
+            success:true,
+            message:"User retrieved successfully",
+            data: result.rows[0],
+        })
+    }catch(error:any){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+            error:error,
+        })
+    }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
