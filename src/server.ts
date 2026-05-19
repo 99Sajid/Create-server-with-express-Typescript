c12f391eaff5a0f874394bad1073887b22ecb2df
Md. Shajid Rahman <shajiedrahman@gmail.com>
2026-05-20 00:33:00 +0600

    updated Delete Method

--- src/server.ts
+++ src/server.ts
@@ -120,6 +120,32 @@ app.put("/api/users/:id",async(req:Request,res:Response)=>{
     }
 });
 
+app.delete("/api/users/:id",async(req:Request,res:Response)=>{
+    const {id}=req.params;
+    try{
+        const result=await pool.query(`
+        DELETE FROM users WHERE id=$1
+        `,[id]);
+        if(result.rowCount===0){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            });
+        }
+        res.status(200).json({
+            success:true,
+            message:"User deleted successfully",
+            data:{},
+        })
+    }catch(error:any){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+            error:error,
+        })
+    }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
